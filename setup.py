018135de29a293f30a99e54806a521664474d370
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-13 12:24:44 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="MicroCompiler",
+    version="0.0.1",
+    packages=[
+        "MicroCompiler",
+        "MicroCompiler.Lookahead",
+        "MicroCompiler.ParserGenerator",
+    ],
+    url="https://github.com/howl-anderson/MicroCompiler",
+    license="MIT",
+    author="Xiaoquan Kong",
+    install_requires=["pyyaml"],
+    author_email="u1mail2me@gmail.com",
+    description="A micro compiler project to provide LL/LR/LALR syntax parser",
+)
