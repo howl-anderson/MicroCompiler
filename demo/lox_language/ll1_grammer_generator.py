10e545a56035e60d3800b93d366a6944cef3273d
Xiaoquan Kong <u1mail2me@gmail.com>
2019-10-20 14:26:41 +0800

    Update lox language grammar file

--- demo/lox_language/ll1_grammer_generator.py
+++ demo/lox_language/ll1_grammer_generator.py
@@ -1,6 +1,6 @@
 from MicroCompiler.ParserGenerator.Generator import Generator
 
-g = Generator("lox_grammar.mbnf")
+g = Generator("lox_official_grammar.mbnf")
 g.generate()
 
 g.write_yaml("lox_grammar.yaml")
